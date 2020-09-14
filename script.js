d3d01556db67e94b57f41d18b6a30e4ea9c43fc5
Kota Matsumoto <mac@mba.local>
2020-09-14 23:20:40 +0900

    インスタンスを作成

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 {
-  class panel {
+  class Panel {
     constructor() {
       const section = document.createElement('section');
       section.classList.add('panel');
@@ -12,7 +12,19 @@
       this.stop = document.createElement('div');
       this.stop.textContent = 'STOP';
       this.stop.classList.add('stop');
+
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+
+      const main = document.querySelector('main');
+      main.appendChild(section);
+
     }
   }
 
+  const panels = [
+    new Panel(),
+    new Panel(),
+    new Panel(),
+  ];
 }
